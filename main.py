69cdfc92d23aba0e133484e297e5b750d03f0124
NORMABENITEZ504 <normaceciliabenitez3@gmail.com>
2026-04-05 19:46:12 -0600

    Restore and update social media section in main.py

--- main.py
+++ main.py
@@ -156,26 +156,24 @@ with col_deezer: # Columna derecha: Deezer
 
 st.divider()
 
-# --- SECCIÓN REDES SOCIALES (2 COLUMNAS) ---
-
+# --- SECCIÓN REDES SOCIALES (RESTAURADA EXACTAMENTE COMO LA QUERÍAS) ---
 left, right = st.columns(2)
 
 with left:
-
-    st.markdown("### 🎧 Perfiles Oficiales")
-
-    st.write("[Spotify BTS](https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX) | [YouTube](https://www.youtube.com/@BANGTANTV)")
-
-    st.write("[Apple Music](https://music.apple.com/artist/bts/667061285) | [Deezer](https://www.deezer.com/artist/4105021)")
+    st.markdown("### Perfiles Oficiales")
+    st.markdown("- [Spotify: BTS](https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX)")
+    st.markdown("- [YouTube: BANGTANTV](https://www.youtube.com/@BANGTANTV)")
+    st.markdown("- [Apple Music: BTS](https://music.apple.com/artist/bts/667061285)")
+    st.markdown("- [Deezer: BTS](https://www.deezer.com/artist/4105021)")
+    st.write("**Spotify Solistas:** [JK](https://open.spotify.com/artist/6Ha9SArjAue9uRMQH13T9t) | [Jimin](https://open.spotify.com/artist/1pYpS9vNUn0949YpU66pBy) | [V](https://open.spotify.com/artist/3JsHnS6YvS6vS9z6S6vS9z) | [RM](https://open.spotify.com/artist/2EcnidS8vL936NidS8vL93) | [Jin](https://open.spotify.com/artist/5pYpS9vNUn0949YpU66pBy) | [Suga](https://open.spotify.com/artist/0b1sIQumIAsNbqAoIClSpy) | [j-hope](https://open.spotify.com/artist/)")
 
 with right:
-
-    st.markdown("### 📱 Redes Sociales")
-
-    st.write("[Instagram](https://www.instagram.com/bts.bighitofficial) | [X](https://x.com/bts_bighit) | [TikTok](https://www.tiktok.com/@bts_official_bighit)")
-
-    st.caption("RM | Jin | SUGA | j-hope | Jimin | V | JK")
-
-
+    st.markdown("### Redes Sociales")
+    st.markdown("- [Instagram: @bts.bighitofficial](https://www.instagram.com/bts.bighitofficial)")
+    st.markdown("- [X (Twitter): @bts_bighit](https://x.com/bts_bighit)")
+    st.markdown("- [TikTok: @bts_official_bighit](https://www.tiktok.com/@bts_official_bighit)")
+    st.write("**Instagram Miembros:**")
+    st.caption("[RM](https://www.instagram.com/rkive) | [Jin](https://www.instagram.com/jin) | [SUGA](https://www.instagram.com/agustd) | [j-hope](https://www.instagram.com/uarmyhope) | [Jimin](https://www.instagram.com/j.m) | [V](https://www.instagram.com/thv) | [JK](https://www.instagram.com/mnijungkook)")
 
 st.caption("Creado para ARMY Honduras 💜")
+
